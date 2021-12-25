1b8b17b6e02a503eefd5a4d6a6a9e161bf38028e
mtstnt <matthew.sutanto0612@yahoo.com>
2021-12-25 22:08:00 +0700

    Day2 part 2 done, simplify part 1 with maps instead of list

--- Day2_1.ex
+++ Day2_1.ex
@@ -1,21 +1,15 @@
 defmodule Day2.Counter do
   def start(str), do: str |> String.trim("\r") |> String.split(" ") |> walk
-  defp walk(["forward", n]), do: [String.to_integer(n), 0]
-  defp walk(["down", n]), do: [0, String.to_integer(n)]
-  defp walk(["up", n]), do: [0, -String.to_integer(n)]
+  defp walk(["forward", n]), do: %{h: String.to_integer(n), d: 0}
+  defp walk(["down", n]), do: %{h: 0, d: String.to_integer(n)}
+  defp walk(["up", n]), do: %{h: 0, d: -String.to_integer(n)}
 end
 
 {:ok, input} = File.read("files/day2.txt")
 result = input
 |> String.split("\n")
-|> Enum.reduce([0, 0], fn (el, acc) ->
+|> Enum.reduce(%{h: 0, d: 0}, fn (el, acc) ->
   result = Day2.Counter.start(el)
-  if !is_list(acc) do
-    result
-  else
-    Enum.map(0..1, fn el ->
-      Enum.at(acc, el) + Enum.at(result, el)
-    end)
-  end
+  %{h: acc.h + result.h, d: acc.d + result.d}
 end)
-IO.puts Enum.at(result, 0) * Enum.at(result, 1)
+IO.puts result.h * result.d
--- Day2_2.ex
+++ Day2_2.ex
@@ -1,21 +1,15 @@
 defmodule Day2.Counter do
-  def start(str), do: str |> String.trim("\r") |> String.split(" ") |> walk
-  defp walk(["forward", n]), do: [String.to_integer(n), 0]
-  defp walk(["down", n]), do: [0, String.to_integer(n)]
-  defp walk(["up", n]), do: [0, -String.to_integer(n)]
+  def start(str, aim), do: str |> String.trim("\r") |> String.split(" ") |> walk(aim)
+  defp walk(["forward", n], aim), do: %{h: String.to_integer(n), d: String.to_integer(n) * aim, a: 0}
+  defp walk(["down", n], _),    do: %{h: 0, d: 0, a: String.to_integer(n)}
+  defp walk(["up", n], _),      do: %{h: 0, d: 0, a: -String.to_integer(n)}
 end
 
 {:ok, input} = File.read("files/day2.txt")
 result = input
 |> String.split("\n")
-|> Enum.reduce([0, 0], fn (el, acc) ->
-  result = Day2.Counter.start(el)
-  if !is_list(acc) do
-    result
-  else
-    Enum.map(0..1, fn el ->
-      Enum.at(acc, el) + Enum.at(result, el)
-    end)
-  end
+|> Enum.reduce(%{h: 0, d: 0, a: 0}, fn (el, acc) ->
+  result = Day2.Counter.start(el, acc.a)
+  %{h: acc.h + result.h, d: acc.d + result.d, a: acc.a + result.a}
 end)
-IO.puts Enum.at(result, 0) * Enum.at(result, 1)
+IO.puts result.h * result.d
